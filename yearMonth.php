1bc3eb624f7b7168f44d6ae6be67fe809f084c92
MrEuclid <john@teacherjohn.org>
2026-03-29 16:41:53 +0700

    Add files via upload

--- yearMonth.php
+++ yearMonth.php
@@ -2,20 +2,26 @@
   $year = date("Y") ;
 $month = date("m") ;
 $day = date("d");
- $y = $year;
+ 
 if ($month > 9) 
   {
     $y = $year ; // keeps real year
     $year = $year + 1; 
-
+// $schoolYear = $year;
   } 
 
-/*
-  echo $month . "<br>" ;
 if ($month < 9)
-{echo $y; } 
-else {echo $year;}
-echo "<br>";
+{
+// $y = $year -1 ;
+ $y = $year -1;
 
-*/
-    ?>
+}
+
+if ($month == 9)
+{
+  $y = $year  ;
+}
+$schoolYear = $year;
+$currentYear = $y;
+
+// echo $currentYear . ' ' . $schoolYear ;
