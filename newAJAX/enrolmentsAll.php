8e21fbb8784921d2db460631fdd3a89c8df8d340
John Thompson <john@teacherjohn.org>
2026-04-01 09:33:05 +0700

    replace wrong copy

--- newAJAX/enrolmentsAll.php
+++ newAJAX/enrolmentsAll.php
@@ -27,3 +27,32 @@ echo  $allTime;
 
 
 ?>
+<?php
+
+include "../connectDatabase.php" ;
+
+
+$q = "SELECT MAX(Year) FROM New_ID_Year_Grade";
+$r = mysqli_query($dbServer,$q);
+$d = mysqli_fetch_row($r);
+$year = $d[0] ;
+
+$query = "SELECT count(ID) AS N
+FROM `New_ID_Year_Grade` 
+WHERE Year = '$year'
+AND Student_ID NOT IN (SELECT id FROM New_Students WHERE GONE = 'Y')" ;
+
+$result = mysqli_query($dbServer,$query);
+$data = mysqli_fetch_row($result);
+$thisYear = $data[0];
+// get all enrolments since 2015
+
+$query = "SELECT count(DISTINCT Student_ID) AS N FROM New_ID_Year_Grade";
+$result = mysqli_query($dbServer,$query);
+$data = mysqli_fetch_row($result);
+$allTime = $data[0] ; // since 2015
+echo  $allTime;
+
+
+
+?>
